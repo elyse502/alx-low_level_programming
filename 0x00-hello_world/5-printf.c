7bdaa6724a203d9ec267245bc72584d0ad17a069
Elysee <elyseniyibizi502@gmail.com>
2023-03-16 06:12:33 -0700

    Printf c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - my main function
+ *
+ * Description: this prints some string
+ * and returns 0.
+ * Return: always returns 0 (success)
+ *
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
+
