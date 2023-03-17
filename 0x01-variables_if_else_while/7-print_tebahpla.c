e011c2440a0f1f6dac6bf531c2324cedd70f5d2b
Elysee <elyseniyibizi502@gmail.com>
2023-03-17 06:53:52 -0700

    New Commit

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - my main
+ * Description: hello
+ * Return: yesss
+ */
+
+int main(void)
+{
+	char n;
+
+	for (n = 'z'; n >= 'a'; n--)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
