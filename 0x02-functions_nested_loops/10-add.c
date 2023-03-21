eb4d03005f545474e4ea3dd50619b3a92560b91d
Elysee <elyseniyibizi502@gmail.com>
2023-03-21 07:43:58 -0700

    Adding

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * add - main fuction
+ * Description: my description
+ * @num1: hello
+ * @num2: hello
+ * Return: 0
+ */
+
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
