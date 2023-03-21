d87f095f329d03e6ad0146a980a6ce43e6b045a9
Elysee <elyseniyibizi502@gmail.com>
2023-03-21 09:21:16 -0700

    Times Table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -13,32 +13,32 @@
 
 void do_work(int index, int num, int n, int ref)
 {
-        if (num < 100)
-        {
-                _putchar(num % 10 + '0');
-        } else
-        {
-                _putchar((((num % 100) - ((num % 100) % 10)) / 10) + '0');
-                _putchar(num % 10 + '0');
-        }
-        if (index < n)
-        {
-                int to_come = (index + 1) * ref;
+	if (num < 100)
+	{
+		_putchar(num % 10 + '0');
+	} else
+	{
+		_putchar((((num % 100) - ((num % 100) % 10)) / 10) + '0');
+		_putchar(num % 10 + '0');
+	}
+	if (index < n)
+	{
+		int to_come = (index + 1) * ref;
 
-                _putchar(',');
-                if (to_come < 10)
-                {
-                        _putchar(' ');
-                }
-                if (to_come < 100)
-                {
-                        _putchar(' ');
-                        _putchar(' ');
-                } else
-                {
-                        _putchar(' ');
-                }
-        }
+		_putchar(',');
+		if (to_come < 10)
+		{
+			_putchar(' ');
+		}
+		if (to_come < 100)
+		{
+			_putchar(' ');
+			_putchar(' ');
+		} else
+		{
+			_putchar(' ');
+		}
+	}
 }
 
 /**
@@ -50,35 +50,30 @@ void do_work(int index, int num, int n, int ref)
 
 void print_times_table(int n)
 {
-        int ref, index;
+	int ref, index;
 
-        if (n > 15 || n < 0)
-        {
-                return;
-        }
+	if (n > 15 || n < 0)
+	{
+		return;
+	}
 
-        for (ref = 0; ref <= n; ref++)
-        {
-                for (index = 0; index <= n; index++)
-                {
-                        int  num = index * ref;
+	for (ref = 0; ref <= n; ref++)
+	{
+		for (index = 0; index <= n; index++)
+		{
+			int  num = index * ref;
 
-                        int first_num;
+			int first_num;
 
-                        if (num < 100)
-                        {
-                                first_num = (num - (num % 10)) / 10;
-                        } else
-                        {
-                                first_num = (num - (num % 100)) / 100;
-                        }
+			if (num < 100)
+				first_num = (num - (num % 10)) / 10;
+			else
+				first_num = (num - (num % 100)) / 100;
 
-                        if (first_num != 0)
-                        {
-                                _putchar(first_num + '0');
-                        }
-                        do_work(index, num, n, ref);
-                }
-                _putchar('\n');
-        }
+			if (first_num != 0)
+				_putchar(first_num + '0');
+			do_work(index, num, n, ref);
+		}
+		_putchar('\n');
+	}
 }
