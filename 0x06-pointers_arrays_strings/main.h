04cb91a1fbf9472afcc08d8b9f2e9e82b63b80d0
Elysee <elyseniyibizi502@gmail.com>
2023-03-29 12:18:57 -0700

    PrintBuffer.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -6,6 +6,7 @@ int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
+char *leet(char *);
 char *rot13(char *);
 void print_number(int n);
 char *add_strings(char *n1, char *n2, char *r, int r_index);
