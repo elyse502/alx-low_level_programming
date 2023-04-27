0aa3a46cf6229ec8a30117afe372c7d774d42d8d
elyse502 <elyseniyibizi502@gmail.com>
2023-04-27 06:25:47 -0700

    Listlen.c

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * list_len - Finds the number of elements in a list.
+ * @h: Singly linked list.
+ * @elements: A counter var for size_t (nodes).
+ * Return: Number of elements in the list h.
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t NumberOfNodes;
+
+	NumberOfNodes = 0;
+	while (h != NULL)
+	{
+		h = h->next;
+		NumberOfNodes++;
+	}
+	return (NumberOfNodes);
+}
