a89ba065050e1b396f73e463ee28524cb747853f
elyse502 <elyseniyibizi502@gmail.com>
2023-04-27 07:43:19 -0700

    First.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- * First -Function that prints a string before the
+ * First - Function that prints a string before the
  *        main Function is executed.
  * Return: no return.
  */
 
 void __attribute__ ((constructor)) First()
 {
-	printf("You're beat! and yet, you must allow\n");
-	printf(",I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow");
+	printf(",\nI bore my house upon my back!\n");
 }
