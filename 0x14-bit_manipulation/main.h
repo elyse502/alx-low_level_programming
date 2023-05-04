3effd97c7ed0ed5352bc4e4671e56b30c5bcdbe8
elyse502 <elyseniyibizi502@gmail.com>
2023-05-04 06:38:52 -0700

    BinaryToUint.c

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _MAIN_H_
+#define _MAIN_H_
 /*
  * Desc: Header file containing prototypes for all Functions
  *       written in the 0x13-bit_manipulation Directory.
@@ -14,4 +14,4 @@ int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int get_endianness(void);
 
-#endif
+#endif 
