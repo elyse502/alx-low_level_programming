5e358d133b88f80655c90537586eba0cf0db8884
elyse502 <elyseniyibizi502@gmail.com>
2023-06-19 04:33:05 -0700

    Contains C functions that can be called from Python

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,50 @@
+/**
+ * add - Addition.
+ * @a: First number.
+ * @b: Second number.
+ * Return: result.
+ */
+int add(int a, int b)
+{
+      return (a + b);
+}
+/**
+ * sub - Substraction.
+ * @a: First number.
+ * @b: Second number
+ * Return: result.
+ */
+int sub(int a, int b)
+{
+      return (a - b);
+}
+/**
+ * mul - Multiplication.
+ * @a: First number.
+ * @b: Second number.
+ * Return: result.
+ */
+int mul(int a, int b)
+{
+      return (a * b);
+}
+/**
+ * div - Division.
+ * @a: First number.
+ * @b: Second number.
+ * Return: result.
+ */
+int div(int a, int b)
+{
+      return (a / b);
+}
+/**
+ * mod - Modulus operartor(Remainder).
+ * @a: First number.
+ * @b: Second number.
+ * Return: result.
+ */
+int mod(int a, int b)
+{
+      return (a % b);
+}
