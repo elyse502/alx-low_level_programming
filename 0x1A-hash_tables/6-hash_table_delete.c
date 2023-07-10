8438d804fb79ed980a7765bf26afad8a361a0f8a
elyse502 <elyseniyibizi502@gmail.com>
2023-07-10 02:55:48 -0700

    Deletes a hash table

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - Frees & Deletes a hash table.
+ * @ht: A pointer to a hash table.
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_table_t *head = ht;
+	hash_node_t *node, *tmp;
+	unsigned long int idx;
+
+	for (idx = 0; idx < ht->size; idx++)
+	{
+		if (ht->array[idx] != NULL)
+		{
+			node = ht->array[idx];
+			while (node != NULL)
+			{
+				tmp = node->next;
+				free(node->key);
+				free(node->value);
+				free(node);
+				node = tmp;
+			}
+		}
+	}
+	free(head->array);
+	free(head);
+}
